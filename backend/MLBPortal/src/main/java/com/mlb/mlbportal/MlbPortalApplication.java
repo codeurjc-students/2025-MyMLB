775ce09a5daff92a194a0769ea6c1f83257def74
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-09-21 12:31:45 +0200

    Add GraalVM configs and generate the binary of the backend

---
+++ backend/MLBPortal/src/main/java/com/mlb/mlbportal/MlbPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.mlbportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlbPortalApplication.class, args);
+    }
+
+}
