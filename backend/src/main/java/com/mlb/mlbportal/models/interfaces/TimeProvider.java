ded4af07c0044515d0465196883ea5d700e536fa
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-10-23 21:36:37 +0200

    Remove MatchControllerTest

--- backend/src/main/java/com/mlb/mlbportal/models/interfaces/TimeProvider.java
+++
@@ -1,9 +0,0 @@
-package com.mlb.mlbportal.models.interfaces;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-public interface TimeProvider {
-    LocalDateTime now();
-    LocalDate today();
-}
