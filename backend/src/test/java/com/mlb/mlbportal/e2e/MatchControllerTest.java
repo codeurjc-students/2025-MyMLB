55555a4188ea59399c08a415c02dfa3931f98014
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-10-23 17:13:50 +0200

    Fix Backend E2E Tests

--- backend/src/test/java/com/mlb/mlbportal/e2e/MatchControllerTest.java
+++ backend/src/test/java/com/mlb/mlbportal/e2e/MatchControllerTest.java
@@ -31,7 +31,9 @@ import static com.mlb.mlbportal.utils.TestConstants.TEST_TEAM3_LOSSES;
 import static com.mlb.mlbportal.utils.TestConstants.TEST_TEAM3_NAME;
 import static com.mlb.mlbportal.utils.TestConstants.TEST_TEAM3_WINS;
 
+import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -42,11 +44,14 @@ public class MatchControllerTest extends BaseE2ETest {
     @SuppressWarnings("unused")
     void setUp() {
         cleanDatabase();
-        this.team1 = saveTestTeam(TEST_TEAM1_NAME, TEST_TEAM1_ABBREVIATION, TEST_TEAM1_WINS, TEST_TEAM1_LOSSES, League.AL,
+        this.team1 = saveTestTeam(TEST_TEAM1_NAME, TEST_TEAM1_ABBREVIATION, TEST_TEAM1_WINS, TEST_TEAM1_LOSSES,
+                League.AL,
                 Division.EAST, TEST_TEAM1_LOGO);
-        this.team2 = saveTestTeam(TEST_TEAM2_NAME, TEST_TEAM2_ABBREVIATION, TEST_TEAM2_WINS, TEST_TEAM2_LOSSES, League.AL,
+        this.team2 = saveTestTeam(TEST_TEAM2_NAME, TEST_TEAM2_ABBREVIATION, TEST_TEAM2_WINS, TEST_TEAM2_LOSSES,
+                League.AL,
                 Division.EAST, TEST_TEAM2_LOGO);
-        this.team3 = saveTestTeam(TEST_TEAM3_NAME, TEST_TEAM3_ABBREVIATION, TEST_TEAM3_WINS, TEST_TEAM3_LOSSES, League.NL,
+        this.team3 = saveTestTeam(TEST_TEAM3_NAME, TEST_TEAM3_ABBREVIATION, TEST_TEAM3_WINS, TEST_TEAM3_LOSSES,
+                League.NL,
                 Division.CENTRAL, TEST_TEAM3_LOGO);
 
         saveTestMatches(team1, team2, 0, 0, LocalDateTime.now().plusMinutes(5), MatchStatus.Scheduled);
@@ -58,15 +63,18 @@ public class MatchControllerTest extends BaseE2ETest {
     @DisplayName("GET /api/matches/today should return all the matches scheduled for today")
     void testGetMatchesOfTheDay() {
         given()
-        .when()
-        .get(MATCHES_OF_DAY_PATH)
-        .then()
-        .statusCode(200)
-        .body("content.size()", is(3))
-        .body("content.homeTeam.name", hasItems(TEST_TEAM1_NAME, TEST_TEAM2_NAME, TEST_TEAM3_NAME))
-        .body("content.awayTeam.name", hasItems(TEST_TEAM1_NAME, TEST_TEAM2_NAME, TEST_TEAM3_NAME))
-        .body("content.status", hasItems("Scheduled", "InProgress", "Finished"))
-        .body("page.totalElements", is(3))
-        .body("page.size", is(10));
+                .baseUri(RestAssured.baseURI)
+                .port(this.port)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(MATCHES_OF_DAY_PATH)
+                .then()
+                .statusCode(200)
+                .body("content.size()", is(3))
+                .body("content.homeTeam.name", hasItems(TEST_TEAM1_NAME, TEST_TEAM2_NAME, TEST_TEAM3_NAME))
+                .body("content.awayTeam.name", hasItems(TEST_TEAM1_NAME, TEST_TEAM2_NAME, TEST_TEAM3_NAME))
+                .body("content.status", hasItems("Scheduled", "InProgress", "Finished"))
+                .body("page.totalElements", is(3))
+                .body("page.size", is(10));
     }
 }
