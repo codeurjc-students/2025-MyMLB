93e7c6d1b456f05254ffb51c3e29cdb82b6c3fc4
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-10-24 18:16:25 +0200

    First steps of the frontend implementation

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-profile',
@@ -8,12 +9,27 @@ import { AuthService } from '../../services/auth.service';
 })
 export class ProfileComponent implements OnInit {
 	public username = '';
+	public errorMessage = '';
 
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService, private router: Router) {}
 
 	ngOnInit(): void {
 		this.authService.getActiveUser().subscribe(response => {
 			this.username = response.username;
+		});
+	}
+
+	public logoutButton() {
+		this.authService.logoutUser().subscribe({
+			next: (response) => {
+				if (response.status === 'SUCCESS') {
+					this.router.navigate(['/']);
+				}
+				else {
+					this.errorMessage = response.message;
+				}
+			},
+			error: (err) => this.errorMessage = err.message
 		});
 	}
 }
