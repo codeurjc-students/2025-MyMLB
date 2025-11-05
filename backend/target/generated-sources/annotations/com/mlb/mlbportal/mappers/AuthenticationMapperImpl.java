e418b64140977aff3d20affbe280680330d75c51
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-11-05 20:13:21 +0100

    Adjust Tests

--- backend/target/generated-sources/annotations/com/mlb/mlbportal/mappers/AuthenticationMapperImpl.java
+++ backend/target/generated-sources/annotations/com/mlb/mlbportal/mappers/AuthenticationMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-05T17:33:35+0100",
+    date = "2025-11-05T20:11:43+0100",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.44.0.v20251023-0518, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
