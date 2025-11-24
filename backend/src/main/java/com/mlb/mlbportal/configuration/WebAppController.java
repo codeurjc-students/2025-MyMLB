64d42a37b38af585e5bde93ad06cb23862519863
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-11-24 19:00:16 +0100

    Fix WebAppController mapping pattern

--- backend/src/main/java/com/mlb/mlbportal/configuration/WebAppController.java
+++ backend/src/main/java/com/mlb/mlbportal/configuration/WebAppController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebAppController {
 
-    @RequestMapping({
-        "/{path:[^\\.]*}",
-        "/**/{path:[^\\.]*}"
-    })
+    @RequestMapping("/{path:[^\\.]*}")
     public String forward() {
         return "forward:/index.html";
     }
