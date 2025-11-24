28cf683b90045818d7f7fba28420ea34ed02731e
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-11-24 19:13:36 +0100

    Fix sonar bad smells

--- backend/src/main/java/com/mlb/mlbportal/configuration/WebAppController.java
+++ backend/src/main/java/com/mlb/mlbportal/configuration/WebAppController.java
@@ -1,13 +1,14 @@
 package com.mlb.mlbportal.configuration;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebAppController {
 
     @RequestMapping("/{path:[^\\.]*}")
-    public String forward() {
+    public String forward(@PathVariable("path") String unused) {
         return "forward:/index.html";
     }
 }
