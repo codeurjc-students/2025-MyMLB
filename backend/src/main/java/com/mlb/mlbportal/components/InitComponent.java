6a4b110d3f8c833778b3f9a93f5af06f905f1d26
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2026-01-18 20:46:47 +0100

    Fix Integration Tests CI Attempt1

--- backend/src/main/java/com/mlb/mlbportal/components/InitComponent.java
+++ backend/src/main/java/com/mlb/mlbportal/components/InitComponent.java
@@ -2,6 +2,7 @@ package com.mlb.mlbportal.components;
 
 import com.mlb.mlbportal.services.MatchService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class InitComponent {
 
     private final DataInitializerService dataInitializerService;
