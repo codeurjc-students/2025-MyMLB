5cbdb94e239212d0eafaf07b717fe60804078e0c
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2026-02-26 22:58:26 +0100

    Improve JPA Queries with EntityGraph

--- backend/src/main/java/com/mlb/mlbportal/repositories/ticket/EventManagerRepository.java
+++ backend/src/main/java/com/mlb/mlbportal/repositories/ticket/EventManagerRepository.java
@@ -3,10 +3,7 @@ package com.mlb.mlbportal.repositories.ticket;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -20,7 +17,8 @@ public interface EventManagerRepository extends JpaRepository<EventManager, Long
     @Query("SELECT em FROM EventManager em WHERE em.id = :id")
     Optional<EventManager> findByIdWithBlock(@Param("id")Long id);
 
-    @Query("SELECT em FROM EventManager em JOIN FETCH em.sector WHERE em.event.id = :id AND em.availability > 0")
+    @EntityGraph(attributePaths = {"sector"})
+    @Query("SELECT em FROM EventManager em WHERE em.event.id = :id AND em.availability > 0")
     List<EventManager> findAvailableSectors(@Param("id")Long id);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
