e96e98795bee9fa14269edde7f3a87f1f11ba535
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2026-03-05 23:47:12 +0100

    Test CI triggers

--- backend/src/main/java/com/mlb/mlbportal/models/ticket/Event.java
+++ backend/src/main/java/com/mlb/mlbportal/models/ticket/Event.java
@@ -1,14 +1,24 @@
 package com.mlb.mlbportal.models.ticket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mlb.mlbportal.models.Match;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "T_Event")
