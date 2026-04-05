b707825960c153647f7c29ef72991e040339ea05
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2026-04-05 15:48:59 +0200

    Fix tetss

--- backend/src/test/java/com/mlb/mlbportal/integration/player/PlayerServiceWriteOperationsIntegrationTest.java
+++ backend/src/test/java/com/mlb/mlbportal/integration/player/PlayerServiceWriteOperationsIntegrationTest.java
@@ -86,7 +86,8 @@ class PlayerServiceWriteOperationsIntegrationTest {
         );
         PlayerDTO result = this.playerService.createPlayer("position", request);
 
-        assertThat(result).isNotNull().isInstanceOf(PositionPlayerDTO.class).isEqualTo(NEW_PLAYER_NAME);
+        assertThat(result).isNotNull().isInstanceOf(PositionPlayerDTO.class);
+        assertThat(result.name()).isEqualTo(NEW_PLAYER_NAME);
 
         this.playerRepository.flush();
 
